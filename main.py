a3b26e83b29984ba3227c5769b85678e3c98aded
Sean Bannon <seannbannon@gmail.com>
2022-05-23 23:15:13 -0500

    you can now guess the icmouters number. wow python seems a lot easier than js

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
-        guess = input('Guess a number netween 1 and {x}')
-        print(guess)
+        guess = int(input(f'Guess a number netween 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, guess again. Too low!')
+        elif guess > random_number:
+            print('Guess again, sucka. Too high.')
+
+
+    print('Yo YOU GUESSED THE RIGHT NUMBER! IT WAS {random_number}!')
 
 guess(10)
